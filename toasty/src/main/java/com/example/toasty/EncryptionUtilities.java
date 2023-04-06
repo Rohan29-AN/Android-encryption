2aa2c79f5bb9c6957b37bfdbddbe5f85cf022912
Dev Vanilla <mgvanilla@gmail.com>
2023-04-06 21:24:05 +0300

    feat: Utilities RSA

---
+++ toasty/src/main/java/com/example/toasty/EncryptionUtilities.java
@@ -0,0 +1,29 @@
+package com.example.toasty;
+
+public class EncryptionUtilities {
+
+    /**
+     * @param isEncryption true if the key is for encryption, false if it is for decryption
+     * @param value word to encrypt or decrypt
+     * @param key the key string (public key if isEncryption is true, private key if isEncryption is false)
+     * @return resultat This method returns a string if decryption or encryption goes well and null if a problem occurs
+     */
+    public static String EncryptOrDecryptRSA(Boolean isEncryption,String value,String key){
+        String resultat="";
+        try{
+            RSAEncryption rsaEncryption=new RSAEncryption();
+            rsaEncryption.initializeRSAKeys(isEncryption,key);
+            if(isEncryption) {
+                resultat=rsaEncryption.encryptData(value);
+            }
+            else{
+                resultat=rsaEncryption.decryptData(value);
+            }
+            return resultat;
+        }
+        catch (Exception e){
+            System.out.println("A problem has occurred"+e.getMessage());
+            return null;
+        }
+    }
+}
